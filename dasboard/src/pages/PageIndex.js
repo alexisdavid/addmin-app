92c232aa1f0876c411fe5516bcaee0098b0f1b53
alexisdavid <alexisduarte1512@gmail.com>
2021-11-18 08:15:22 -0500

    combio

--- dasboard/src/pages/PageIndex.js
+++ dasboard/src/pages/PageIndex.js
@@ -8,178 +8,74 @@ const request = new Request();
 
 
 const PageIndex = (props) => {
-    // const {history}=props;
-    // const [credentials,setCredentials] = useState({user:'',password:''})
-    // useEffect(() => {
-    //     const isAuth = window.sessionStorage.getItem("token");
-    //     if (isAuth)  history.push('/home');
-    //   });
-    // const handleChange=(e) =>   setCredentials({...credentials,[e.target.name]:e.target.value})
+    const {history}=props;
+    const [credentials,setCredentials] = useState({user:'',password:''})
+    useEffect(() => {
+        const isAuth = window.sessionStorage.getItem("token");
+        if (isAuth)  history.push('/home');
+      });
+    const handleChange=(e) =>   setCredentials({...credentials,[e.target.name]:e.target.value})
 
-    // const login =async (e)=>{
-    //     e.preventDefault();
-    //     let userInfo;
-    //     const response = await request.post('auth/login',{userCode:credentials.user,password:credentials.password}) 
-    //     if (response && response.statusCode==200 ) {
-    //         let token = {
-    //          user:response.result.user,
-    //           token: response.result.access_token
-    //         };
-    //         let datos = JSON.stringify(token);
-    //         userInfo = base64.encode(datos);
-    //         window.sessionStorage.setItem("token", JSON.stringify(userInfo));
-    //         window.location.replace('/home');
-    //         // history.push('/home')
+    const login =async (e)=>{
+        e.preventDefault();
+        let userInfo;
+        const response = await request.post('auth/login',{userCode:credentials.user,password:credentials.password}) 
+        if (response && response.statusCode==200 ) {
+            let token = {
+             user:response.result.user,
+              token: response.result.access_token
+            };
+            let datos = JSON.stringify(token);
+            userInfo = base64.encode(datos);
+            window.sessionStorage.setItem("token", JSON.stringify(userInfo));
+            window.location.replace('/home');
+            // history.push('/home')
             
-    //       }
-    // }
+          }
+    }
  
     return (
         
      <>
-       <div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-      <div class="col-12">
        
-        <nav class="navbar navbar-expand-lg blur border-radius-xl top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
-          <div class="container-fluid ps-2 pe-0">
-            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
-              Material Dashboard 2
-            </a>
-            <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon mt-2">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </span>
-            </button>
-            <div class="collapse navbar-collapse" id="navigation">
-              <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="../pages/dashboard.html">
-                    <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
-                    Dashboard
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="../pages/profile.html">
-                    <i class="fa fa-user opacity-6 text-dark me-1"></i>
-                    Profile
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="../pages/sign-up.html">
-                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
-                    Sign Up
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="../pages/sign-in.html">
-                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
-                    Sign In
-                  </a>
-                </li>
-              </ul>
-              <ul class="navbar-nav d-lg-block d-none">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/product/material-dashboard" class="btn btn-sm mb-0 me-1 bg-gradient-dark">Free download</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-       
-      </div>
-    </div>
-  </div>
-  <main class="main-content  mt-0">
-    <div class="page-header align-items-start min-vh-100" style={{backgroundImage: "url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80')"}}>
-      <span class="mask bg-gradient-dark opacity-6"></span>
-      <div class="container my-auto">
-        <div class="row">
-          <div class="col-lg-4 col-md-8 col-12 mx-auto">
-            <div class="card z-index-0 fadeIn3 fadeInBottom">
-              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
-                  <div class="row mt-3">
-                    <div class="col-2 text-center ms-auto">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-facebook text-white text-lg"></i>
-                      </a>
-                    </div>
-                    <div class="col-2 text-center px-1">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-github text-white text-lg"></i>
-                      </a>
-                    </div>
-                    <div class="col-2 text-center me-auto">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-google text-white text-lg"></i>
-                      </a>
-                    </div>
-                  </div>
+  <main className="main-content  mt-0">
+    <div className="page-header align-items-start min-vh-100" style={{backgroundImage: "url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80')"}}>
+      <span className="mask bg-gradient-dark opacity-6"></span>
+      <div className="container my-auto">
+        <div className="row">
+          <div className="col-lg-4 col-md-8 col-12 mx-auto">
+            <div className="card z-index-0 fadeIn3 fadeInBottom">
+              <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div className="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                  <h4 className="text-white font-weight-bolder text-center mt-2 mb-0">Iniciar Sesion</h4>
+             
                 </div>
               </div>
-              <div class="card-body">
-                <form role="form" class="text-start">
-                  <div class="input-group input-group-outline my-3">
-                    <label class="form-label">Email</label>
-                    <input type="email" class="form-control"/>
+              <div className="card-body">
+                <form role="form" className="text-start" onSubmit={login}>
+                  <div className="input-group input-group-outline my-3">
+                    <label className="form-label">Usuario</label>
+                    <input type="text" name='user' className="form-control"/>
                   </div>
-                  <div class="input-group input-group-outline mb-3">
-                    <label class="form-label">Password</label>
-                    <input type="password" class="form-control"/>
+                  <div className="input-group input-group-outline mb-3">
+                    <label className="form-label">Password</label>
+                    <input type="password" name='password' className="form-control"/>
                   </div>
-                  <div class="form-check form-switch d-flex align-items-center mb-3">
-                    <input class="form-check-input" type="checkbox" id="rememberMe"/>
-                    <label class="form-check-label mb-0 ms-2" for="rememberMe">Remember me</label>
+                
+                  <div className="text-center">
+                    <button type="submit" className="btn bg-gradient-primary w-100 my-4 mb-2" onClick={login}>Login</button>
                   </div>
-                  <div class="text-center">
-                    <button type="button" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
-                  </div>
-                  <p class="mt-4 text-sm text-center">
-                    Don't have an account?
-                    <a href="../pages/sign-up.html" class="text-primary text-gradient font-weight-bold">Sign up</a>
-                  </p>
+                  <p className="mt-4 text-sm text-center">
+                    No recuerdas tu password??
+                    <span className="text-primary text-gradient font-weight-bold"> contacta a sistemas</span>  
+                  </p> 
                 </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <footer class="footer position-absolute bottom-2 py-2 w-100">
-        <div class="container">
-          <div class="row align-items-center justify-content-lg-between">
-            <div class="col-12 col-md-6 my-auto">
-              <div class="copyright text-center text-sm text-white text-lg-start">
-                © <script>
-                  document.write(new Date().getFullYear())
-                </script>,
-                made with <i class="fa fa-heart" aria-hidden="true"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold text-white" target="_blank">Creative Tim</a>
-                for a better web.
-              </div>
-            </div>
-            <div class="col-12 col-md-6">
-              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com" class="nav-link text-white" target="_blank">Creative Tim</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/presentation" class="nav-link text-white" target="_blank">About Us</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/blog" class="nav-link text-white" target="_blank">Blog</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-white" target="_blank">License</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
+     
     </div>
   </main>
      
